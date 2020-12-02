e6d9e9d50efc4ae8cf160400e41a980aa898e4a1
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-12-03 00:41:25 +0100

    dodana stranica za upload filea na /upload

--- django_project/cti/urls.py
+++ django_project/cti/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='cti-home')
+    path('', views.home, name='cti-home'),
+    path('upload/', views.upload, name='cti-upload')
 ]
--- django_project/cti/views.py
+++ django_project/cti/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'cti/home.html')
 
+def upload(request):
+    return render(request, 'cti/upload.html')
+

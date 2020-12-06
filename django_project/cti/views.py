108f24175f12c538a2bd097bfcee6129cb955cf9
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-12-06 21:50:04 +0100

    dodana funkcionalnost da se uploada filea

--- django_project/cti/views.py
+++ django_project/cti/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 from django.contrib import messages
 from cti.models import IP
-
-# Imaginary function to handle an uploaded file.
-#from somewhere import handle_uploaded_file
+from .models import Apache_log
+from .log_analyzer import analyze
 
 def home(request):
     return render(request, 'cti/home.html')
@@ -16,15 +15,16 @@ def upload(request):
     if request.method == 'GET':
         return render(request, 'cti/upload.html')
     elif request.method == 'POST':
-        print(request)
-        print(request.FILES)
         form = UploadFileForm(request.POST, request.FILES)
-        print(form)
         if form.is_valid():
-            #handle_uploaded_file(request.FILES['file'])
-            messages.success(request, 'Datoteka je spremljena. (Treba implementirati funkciju koja sprema datoteku na file system)')  # <-
+            instance = Apache_log(log_file=request.FILES['file'])
+            instance.save()
+            print(instance.log_file) #ime fajla
+            analyze(instance.log_file)
+
+            messages.success(request, 'File is saved.')
         else:
-            messages.warning(request, 'Desila se greška molimo pokušaje ponovno.')
+            messages.warning(request, 'An error has occured.')
     else:
         form = UploadFileForm()
     return render(request, 'cti/upload.html', {'form': form})

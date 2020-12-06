ab7e42f74102c9c0c6ddce5b638f6b6afcd6440c
Ajdin Trejic <ajdintrejic@protonmail.ch>
2020-12-06 21:52:12 +0100

    dodane informacije u model za ip i dodan model za apache log

--- django_project/cti/models.py
+++ django_project/cti/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
-
 class IP(models.Model):
-    ip_adress = models.CharField(max_length=20)
+    address = models.CharField(max_length=39, null=True)
+    hostname = models.CharField(max_length=64, null=True)
+    city = models.CharField(max_length=64, null=True)
+    region = models.CharField(max_length=64, null=True)
+    country = models.CharField(max_length=32, null=True)
+    countryname = models.CharField(max_length=64, null=True)
+    org = models.CharField(max_length=256, null=True)
+    postal = models.CharField(max_length=16, null=True)
+    timezone = models.CharField(max_length=32, null=True)
+    latitude = models.CharField(max_length=32, null=True)
+    longitude = models.CharField(max_length=32, null=True)
+
+class Apache_log(models.Model):
+    log_file = models.FileField()

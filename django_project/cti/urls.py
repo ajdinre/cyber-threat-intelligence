c193f100f9257791cca8c7fb988a0ea09f0e0389
Ajdin Trejic <ajdintrejic@protonmail.ch>
2021-04-09 19:55:38 +0200

    removed unused url

--- django_project/cti/urls.py
+++ django_project/cti/urls.py
@@ -4,11 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='cti-home'),
     path('upload/', views.upload, name='cti-upload'),
-    path('uploadedfiles/', views.uploaded_files, name='cti-uploaded-files'),
-    path('search/ip/', views.search_ip, name='cti-search-ip'),
-    path('profile/', views.view_profile, name='cti-profile'),
-    path('settings/', views.settings, name='cti-settings'),
-    path('password/', views.change_password, name='cti-changepassword'),
-    path('report/', views.report, name='cti-report'),
-    path('fileinfo/', views.file_info, name='cti-file-info'),
 ]

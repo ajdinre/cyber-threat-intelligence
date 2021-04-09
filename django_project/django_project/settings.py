b6abd3ec8e9e7cc0b905ad1fd93f0d204735739b
Ajdin Trejic <ajdintrejic@protonmail.ch>
2021-04-09 19:57:32 +0200

    updated documentation info

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'user_visit',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -2,11 +2,32 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from django.conf.urls import url
 
 from rest_framework.schemas import get_schema_view
 from rest_framework import routers
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from cti import views as cti_views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
+
 
 #This code generates a REST api
 router = routers.DefaultRouter()
@@ -23,10 +44,17 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     #Documentation
-    path('swagger-ui/', TemplateView.as_view(template_name='cti/swagger-ui.html', extra_context={'schema_url':'openapi-schema'}), name='swagger-ui'),
-    path('openapi/', get_schema_view(title="Cyber Threat Intelligence", description="Api documentation for CTI project.", version="0.1.0-alpha"), name='openapi-schema'),
+    #path('openapi/', get_schema_view(title="Cyber Threat Intelligence", description="Api documentation for CTI project.", version="0.1.0-alpha"), name='openapi-schema'),
+
+    #drf-yasg
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 
     #REST
-    path('log/upload', cti_views.FileUploadView.as_view())
+    path('log/upload', cti_views.FileUploadView.as_view()),
+    path('ip', cti_views.IPView.as_view())
+
 
 ]

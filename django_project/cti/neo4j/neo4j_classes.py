6475a4f24b8b1ec2238b64182f0e30be79b69a33
Marta Gračner <marta.gracner@fer.hr>
2021-04-09 20:12:40 +0200

    refactoring

--- django_project/cti/neo4j/neo4j_classes.py
+++ django_project/cti/neo4j/neo4j_classes.py
@@ -1,17 +1,16 @@
 from .neo4j_db import Database
 
-#metoda stvara novi cvor u bazi tipa class_name
-# koji ima sve atribute zadane dictionaryjem attributes
-#stvara cvor 'class_name' sa atributima 'attributes'
-#class_name je string, a attributes je dictionary
-def create_node(class_name, attributes):
+
+# creates a node of "type" node_label with properties specified in the dictionary node_properties
+# node_label should be a string (with no spaces in between)
+def create_node(node_label, node_properties):
     i = 0
-    query_string = 'CREATE(label' + ':' + class_name + '{'
-    for attribute in attributes:
+    query_string = 'CREATE(label' + ':' + node_label + '{'
+    for node_property in node_properties:
         if i == 0:
-            query_string += attribute + ': "' + attributes[attribute] + '"'
+            query_string += node_property + ': "' + node_properties[node_property] + '"'
         else:
-            query_string += ', ' + attribute + ': "' + attributes[attribute] + '"'
+            query_string += ', ' + node_property + ': "' + node_properties[node_property] + '"'
         i += 1
     query_string += '}) RETURN label'
 
@@ -19,89 +18,90 @@ def create_node(class_name, attributes):
     return db.query(query_string)
 
 
-#creates a node only if the node with the same name and exact same attributes doesn't already exist
-# if it exists, that node is returned
-#e.g. MERGE (charlie { name: 'Charlie Sheen', age: 10 }) RETURN charlie
-def create_if_not_exist(class_name, attributes):
+# creates a node of "type" node_label with properties specified in the dictionary node_properties
+# but only if a node with the same label and exactly the same properties doesn't already exist
+# if such a node exists the method returns it
+# node_label should be a string (with no spaces in between), node_properties should be a dictionary
+# e.g. MERGE (charlie { name: 'Charlie Sheen', age: 10 }) RETURN charlie
+def create_node_if_not_exist(node_label, node_properties):
     i = 0
-    query_string = 'MERGE(label' + ':' + class_name + '{'
-    for attribute in attributes:
+    query_string = 'MERGE(label' + ':' + node_label + '{'
+    for node_property in node_properties:
         if i == 0:
-            query_string += attribute + ': "' + attributes[attribute] + '"'
+            query_string += node_property + ': "' + node_properties[node_property] + '"'
         else:
-            query_string += ', ' + attribute + ': "' + attributes[attribute] + '"'
+            query_string += ', ' + node_property + ': "' + node_properties[node_property] + '"'
         i += 1
     query_string += '}) RETURN label'
 
     db = Database("bolt://localhost:7687", "neo4j", "password")
     return db.query(query_string)
 
-# creates a relationship between first_node_name and second_node_name only if it doesn't already exist
-# if a relationship between nodes with properties first_node and second_node exists nothing happens
-# ALL properties have to match for the database to realise it is a duplicate
-def create_relationship_if_not_exist(first_node_name, first_node, second_node_name, second_node, relationship_name):
+
+# creates a relationship between a node of "type" first_node_label and a node of "type"second_node_label
+# first_node_label, second_node_label and relationship_label should be strings (with no spaces)
+# first_node_properties and second_node_properties should be dictionaries
+def create_relationship(first_node_label, first_node_properties, second_node_label, second_node_properties, relationship_label):
     i = 0
     j = 0
-    query_string = 'MATCH(a:' + first_node_name + '),(b:' + second_node_name + ') WHERE '
-    for attribute in first_node:
+    query_string = 'MATCH(a:' + first_node_label + '),(b:' + second_node_label + ') WHERE '
+    for node_property in first_node_properties:
         if i == 0:
-            query_string += 'a.' + attribute + ' = "' + first_node[attribute] + '"'
+            query_string += 'a.' + node_property + ' = "' + first_node_properties[node_property] + '"'
         else:
-            query_string += ' AND a.' + attribute + ' = "' + first_node[attribute] + '"'
+            query_string += ' AND a.' + node_property + ' = "' + first_node_properties[node_property] + '"'
         i += 1
-    for attribute in second_node:
+    for node_property in second_node_properties:
         if j == 0:
             if i > 0:
                 query_string += ' AND '
-            query_string += 'b.' + attribute + ' = "' + second_node[attribute] + '"'
+            query_string += 'b.' + node_property + ' = "' + second_node_properties[node_property] + '"'
         else:
-            query_string += ' AND b.' + attribute + ' = "' + second_node[attribute] + '"'
+            query_string += ' AND b.' + node_property + ' = "' + second_node_properties[node_property] + '"'
         j += 1
-    query_string += ' MERGE(a)-[r:' + relationship_name + ']->(b) RETURN r'
+    query_string += ' CREATE(a)-[r:' + relationship_label + ']->(b) RETURN r'
 
     db = Database("bolt://localhost:7687", "neo4j", "password")
     return db.query(query_string)
 
 
-#metoda stvara vezu između čvorova tipa first_node_name i second_node_name
-# metoda prvou bazi traži čvorove tih tipova sa atributima zadanim s first_node i second_node
-# ako takvih nema, baca izniku????
-#first_node and second_node are dictionaries
-#first_node_name, second_node_name and relationship_name are strings
-def create_relationship(first_node_name, first_node, second_node_name, second_node, relationship_name):
+# creates a relationship between a node of "type" first_node_label and a node of "type"second_node_label
+# but only if such a relationship (ALL node properties have to match) doesn't already exist
+# if such a relationship already exists nothing happens in the database and the method returns the relationship
+def create_relationship_if_not_exist(first_node_label, first_node_properties, second_node_label, second_node_properties, relationship_label):
     i = 0
     j = 0
-    query_string = 'MATCH(a:' + first_node_name + '),(b:' + second_node_name + ') WHERE '
-    for attribute in first_node:
+    query_string = 'MATCH(a:' + first_node_label + '),(b:' + second_node_label + ') WHERE '
+    for node_property in first_node_properties:
         if i == 0:
-            query_string += 'a.' + attribute + ' = "' + first_node[attribute] + '"'
+            query_string += 'a.' + node_property + ' = "' + first_node_properties[node_property] + '"'
         else:
-            query_string += ' AND a.' + attribute + ' = "' + first_node[attribute] + '"'
+            query_string += ' AND a.' + node_property + ' = "' + first_node_properties[node_property] + '"'
         i += 1
-    for attribute in second_node:
+    for node_property in second_node_properties:
         if j == 0:
             if i > 0:
                 query_string += ' AND '
-            query_string += 'b.' + attribute + ' = "' + second_node[attribute] + '"'
+            query_string += 'b.' + node_property + ' = "' + second_node_properties[node_property] + '"'
         else:
-            query_string += ' AND b.' + attribute + ' = "' + second_node[attribute] + '"'
+            query_string += ' AND b.' + node_property + ' = "' + second_node_properties[node_property] + '"'
         j += 1
-    query_string += ' CREATE(a)-[r:' + relationship_name + ']->(b) RETURN r'
+    query_string += ' MERGE(a)-[r:' + relationship_label + ']->(b) RETURN r'
 
     db = Database("bolt://localhost:7687", "neo4j", "password")
     return db.query(query_string)
 
 
-#metoda vraća sve čvorove tipa class_name s atributima zadanim u attributes
-#klasa je ime čvora, parametri je dictionary sa where parametrima
-def get_nodes(class_name, attributes):
+# returns all nodes of "type" node_label with properties specified in the dictionary node_properties
+# node_label should be a string (with no spaces in between), node_properties should be a dictionary
+def get_nodes(node_label, node_properties):
     i = 0
-    query_string = 'MATCH(label:' + class_name + '{'
-    for attribute in attributes:
+    query_string = 'MATCH(label:' + node_label + '{'
+    for node_property in node_properties:
         if i == 0:
-            query_string += attribute + ': "' + attributes[attribute] + '"'
+            query_string += node_property + ': "' + node_properties[node_property] + '"'
         else:
-            query_string += ', ' + attribute + ': "' + attributes[attribute] + '"'
+            query_string += ', ' + node_property + ': "' + node_properties[node_property] + '"'
         i += 1
     query_string += '}) RETURN label'
 
@@ -110,35 +110,25 @@ def get_nodes(class_name, attributes):
     for res in db.query(query_string):
         result.append(res['label'])
     return result
-#example
-#mydict = {
-#    "datetime": "30/Aug/2020:03:24:31",
-#        "method": "GET"
-#}
-#print(test('Request', mydict))
+
 
 #vraća sve čvorove koji su u bazi
-def get_all():
-    query_string = 'MATCH (a) RETURN (a)'
-    db = Database("bolt://localhost:7687", "neo4j", "password")
-    return db.query(query_string)
+# def get_all():
+#     query_string = 'MATCH (a) RETURN (a)'
+#     db = Database("bolt://localhost:7687", "neo4j", "password")
+#     return db.query(query_string)
 
-#returns a list of properties on the node
-#known_properties is a dictionary of at least one node property
-#e.g. MATCH (a) WHERE a.name = '' RETURN keys(a)
-def get_attributes(known_properties):
+
+# returns a list of nodes properties
+# node_label should be a string representing the "type" of the node
+# e.g. MATCH(a:IP) RETURN keys(a)
+def get_properties_labels(node_label):
     i = 0
-    query_string = 'MATCH(a) WHERE a.'
-    for known_property in known_properties:
-        if i == 0:
-            query_string += known_property + ' = "' + known_properties[known_property] + '"'
-        else:
-            query_string += ' AND a.' + known_property + ' = "' + known_properties[known_property] + '"'
-    query_string += 'RETURN keys(a)'
+    query_string = 'MATCH(a:' + node_label + ') RETURN keys(a)'
 
     db = Database("bolt://localhost:7687", "neo4j", "password")
     result = db.query(query_string)
-    return result[0]['keys(a)']
+    return result
 
 
 # metoda vraća sve requestove koje je poslala ip adresa

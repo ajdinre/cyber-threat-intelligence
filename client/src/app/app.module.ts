a92f33b91777f30bba2fc1caaa78445f29a7f904
Lonky131 <kl50222@fer.hr>
2021-04-15 19:26:24 +0200

    implemented basic profile dropdown menu on the top right,routing works too

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenu, MatMenuModule} from '@angular/material/menu';
 
 //my componentns
 import { NoContentComponent } from './no-content/no-content.component';
@@ -48,6 +49,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MatListModule,
     MatInputModule,
     MatCardModule,
+    MatMenuModule,
     FlexLayoutModule,
     AppRoutingModule
 

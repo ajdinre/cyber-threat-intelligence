28433690a704a9c5da4298884505426e0ad2a4f2
Lonky131 <kl50222@fer.hr>
2021-04-30 17:36:02 +0200

    prepared services and classes for backend integration

---
+++ client/src/app/shared/components/classes/ipadrress.ts
@@ -0,0 +1,9 @@
+export class IpAdrress{
+  id : string;
+  domain? : string;
+  location? : string;
+  latitude? : string;
+  country? : string;
+  countryCode? : string;
+}
+//TODO: Define ip address class with the right parameters!

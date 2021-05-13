baaaa51be24fb742384eca70bf7a25c71c077908
nika.brasnovic <nika.brasnovic@pricefx.com>
2021-05-13 05:47:48 +0200

    Pluggy working Please see log_analyzer line 126 for plugin selection Working when pushed

--- django_project/cti/log_analyzer.py
+++ django_project/cti/log_analyzer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 import os
+import pluggy
+import requests
+import json
 from cti.models import IP
 from cti.models import Log_line
 from cti.models import Apache_log
 from cti.neo4j.neo4j_classes import create_relationship_if_not_exist, create_node_if_not_exist
 
-import ipinfo 
+import ipinfo
 access_token = '5a8dcf646a1d15'
+
 
 def getIPInfo(ip_address):
     global access_token
@@ -15,13 +19,103 @@ def getIPInfo(ip_address):
     return details
 
 
+def getIpStack(ip_address):
+    url = "http://api.ipstack.com/" + ip_address + \
+        "?access_key=f80fa8b45951161b9a0148ab143ccce6"
+    response = requests.request("GET", url)
+    print(response.text)
+    return details
+
+
+hookspec = pluggy.HookspecMarker("cti")
+hookimpl = pluggy.HookimplMarker("cti")
+
+
+class MySpec:
+
+    @hookspec
+    def myhook(self, ip_address):
+        """Hook.
+        """
+
+
+class Plugin_1:
+
+    @hookimpl
+    def myhook(self, ip_address):
+        print("Using ipinfo plugin")
+        global access_token
+        handler = ipinfo.getHandler(access_token)
+        details = handler.getDetails(ip_address)
+        return details
+
+
+class Plugin_2:
+
+    @hookimpl
+    def myhook(self, ip_address):
+        print("Using ipstack plugin")
+        url = "http://api.ipstack.com/" + ip_address + \
+            "?access_key=f80fa8b45951161b9a0148ab143ccce6"
+        response = requests.request("GET", url)
+        parsed_json = (json.loads(response.text))
+        details = {'ip_address': parsed_json['ip'],
+           'hostname': 'No hostname',
+           'city': parsed_json['city'],
+           'region': parsed_json['region_name'],
+           'country': parsed_json['country_code'],
+           'countryname': parsed_json['country_name'],
+           'org': 'No ORG',
+           'postal': parsed_json['zip'],
+           'timezone': 'No timezone',
+           'latitude': parsed_json['latitude'],
+           'longitude': parsed_json['longitude']
+           }
+        return details
+
+class Plugin_3:
+
+    @hookimpl
+    def myhook(self, ip_address):
+        print("Using freegeoip plugin")
+        url = "https://freegeoip.app/json/"+ ip_address
+        headers = {
+            'accept': "application/json",
+            'content-type': "application/json"
+            }
+        response = requests.request("GET", url, headers=headers)
+        parsed_json = (json.loads(response.text))
+        details = {'ip_address': parsed_json['ip'],
+           'hostname': 'No hostname',
+           'city': parsed_json['city'],
+           'region': parsed_json['region_name'],
+           'country': parsed_json['country_code'],
+           'countryname': parsed_json['country_name'],
+           'org': 'No ORG',
+           'postal': parsed_json['zip_code'],
+           'timezone': parsed_json['time_zone'],
+           'latitude': parsed_json['latitude'],
+           'longitude': parsed_json['longitude']
+           }
+        return details
+
+
+# create a manager and add the spec
+pm = pluggy.PluginManager("cti")
+pm.add_hookspecs(MySpec)
+
+
+
+
+
 def analyze(filename, server_data):
+
     os.system('pwd')
     filenameString = str(filename)
     path = 'media/' + filenameString
 
     lines = []
-    with open(path, 'r') as f: 
+    with open(path, 'r') as f:
         lines = f.readlines()
         f.close()
 
@@ -29,6 +123,10 @@ def analyze(filename, server_data):
         lines[i] = lines[i].rstrip()
 
     create_node_if_not_exist('Server', server_data)
+    # register plugins - uncomment the one you want to use
+    # pm.register(Plugin_1())
+    # pm.register(Plugin_2())
+    pm.register(Plugin_3())
 
     for i in range(len(lines)):
         line = lines[i]
@@ -40,8 +138,10 @@ def analyze(filename, server_data):
         httpVersion = details[7][-4:-1]
         response = details[8]
         sizeInBytes = details[9]
+        
 
-        details = getIPInfo(ip_address)
+
+        details = pm.hook.myhook(ip_address = details[0])
 
         ip_attributes = {}
         ip_attributes['ip_address'] = ip_address
@@ -98,16 +198,16 @@ def analyze(filename, server_data):
         }
 
         create_node_if_not_exist('Log_line', log_attributes)
-        create_relationship_if_not_exist('IP', ip_attributes, 'Log_line', log_attributes, 'HAS_SENT')
-        create_relationship_if_not_exist('Log_line', log_attributes, 'Server', server_data, 'HAS_ACCESSED')
+        create_relationship_if_not_exist(
+            'IP', ip_attributes, 'Log_line', log_attributes, 'HAS_SENT')
+        create_relationship_if_not_exist(
+            'Log_line', log_attributes, 'Server', server_data, 'HAS_ACCESSED')
 
         print("filename2 " + str(filenameString))
-        file = Apache_log.objects.get(log_file=filenameString) 
+        file = Apache_log.objects.get(log_file=filenameString)
         file.analyzed = True
         file.save()
 
 
 if __name__ == "__main__":
     main()
-
-

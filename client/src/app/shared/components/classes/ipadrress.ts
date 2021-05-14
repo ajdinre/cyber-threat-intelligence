d1999554e8d1f99168f81fe56bd0ba1ec66a57ce
Lonky131 <kl50222@fer.hr>
2021-05-14 20:55:46 +0200

    connected request result to variables for neo4j

--- client/src/app/shared/components/classes/ipadrress.ts
+++ client/src/app/shared/components/classes/ipadrress.ts
@@ -11,5 +11,20 @@ export class IpAddress{
   longitude? : string;
 
 }
+export class IpAddressNodes{
+  ip_address : string;
+  hostname? : string;
+  org? : string;
+  city? : string;
+  region? : string;
+  country? : string;
+  postal? : string;
+  timezone? : string;
+  latitude? : string;
+  longitude? : string;
+  name?: string;
+  groups?: string;
+  count?: string;
+}
 //TODO: Define ip address class with the right parameters!
 
